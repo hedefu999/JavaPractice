3cc577a022e74c132f10caadac0fee8631d0e3b1
hedefu <hedefu@changingedu.com>
2020-08-12 10:11:22 +0800

    boot+vue学习项目-微人事系统

---
+++ microhumanresource/about.md
@@ -0,0 +1,3 @@
+springboot vue 前后端分离项目
+
+来自《springboot vue实战》
---
+++ microhumanresource/src/main/java/com/microhumanresource/MicrohumanresourceApplication.java
@@ -0,0 +1,13 @@
+package com.microhumanresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrohumanresourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrohumanresourceApplication.class, args);
+    }
+
+}
